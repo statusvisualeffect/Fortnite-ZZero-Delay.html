798b2aa8d1670efac263de345d05a02ee80d1798
statusvisualeffect <154653760+statusvisualeffect@users.noreply.github.com>
2024-01-23 02:46:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('installButton').addEventListener('click', function () {
 	// Replace 'your-install-link' with the actual link you want to open
-	window.location.href = 'https://github.com/LouSkull/Fortnite-0-delay'
+	window.location.href = 'https://drive.google.com/file/d/1AkJNC0Mq8of02cmKO8oTH3HkN22MrA9T/view?usp=sharing'
 })
 
 document.getElementById('githubButton').addEventListener('click', function () {
-	window.location.href = 'https://drive.google.com/file/d/1AkJNC0Mq8of02cmKO8oTH3HkN22MrA9T/view?usp=sharing'
+	window.location.href = 'https://github.com/LouSkull/Fortnite-0-delay'
 })
